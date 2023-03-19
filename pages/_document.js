fdb44eeebdcedebeeb7bac01f96f8b7202808823
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-03-19 10:05:44 +0100

    kove init

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,226 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+          <link
+            href="/fonts/Cormorant-BoldItalic.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/Cormorant-Italic.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/Cormorant-Bold.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/Cormorant-Light.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/Cormorant-LightItalic.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/Cormorant-Medium.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/Cormorant-MediumItalic.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/Cormorant-Regular.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/Cormorant-SemiBold.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/Cormorant-SemiBoldItalic.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantGaramond-Bold.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantGaramond-BoldItalic.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantGaramond-Italic.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantGaramond-Light.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantGaramond-LightItalic.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantGaramond-Medium.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantGaramond-MediumItalic.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantGaramond-Regular.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantGaramond-SemiBold.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantGaramond-SemiBoldItalic.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantInfant-Bold.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantInfant-BoldItalic.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantInfant-Italic.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantInfant-Light.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantInfant-LightItalic.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantInfant-Medium.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantInfant-MediumItalic.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantInfant-Regular.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantInfant-SemiBold.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/CormorantInfant-SemiBoldItalic.woff2"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          {/* <link
+            href="/fonts/"
+            as="font"
+            rel="preload prefetch"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ pages/home/index.js
@@ -0,0 +1,8 @@
+import s from './home.module.scss' 
+
+export default function Home() {
+
+    return( 
+        <>hello</>
+    )
+}
