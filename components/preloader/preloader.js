86ca9ba17b02345500cebcc230ee5b94d8b9b2a5
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-04-01 05:03:20 +0100

    preloader done

--- components/preloader/preloader.js
+++ components/preloader/preloader.js
@@ -2,15 +2,20 @@ import s from './preloader.module.scss'
 import Marquee from 'react-fast-marquee'
 import cn from 'clsx'
 import GSAP  from "gsap"
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import useMultiRefs from '@/util/multiRef'
 const Preloader = ({}) => {
     const topBlackRef = useRef()
     const bottomBlackRef = useRef()
+    const middleRef = useRef()
     const [multiRef, addMultiRef] = useMultiRefs()
-
+    const [marqueeRef, addMarqueeRef] = useMultiRefs()
+    const [showMiddle, setShowMiddle] = useState(false)
+    const [showTopBottom, setShowTopBottom] = useState(false)
     useEffect(() => {
         const h1 = multiRef()
+        const overInners = marqueeRef()
+        
         const timeline = GSAP.timeline()
         timeline.set(topBlackRef.current, {
             height: "50%"
@@ -20,40 +25,72 @@ const Preloader = ({}) => {
         })
 
         timeline.to(bottomBlackRef.current, {
-            y: 500,
+            height: "33.3333333333333%",
             duration: 1,
             ease: "none"
         }, 1)
 
         timeline.to(topBlackRef.current, {
-            y: -500,
+            height: "33.3333333333333%",
             duration: 1,
             ease: "none"
         }, 1)
+        timeline.to(middleRef.current, {
+            height: "33.3333333333333%",
+            duration: 1,
+            ease: "none"
+        }, 1)
+        
+        timeline.to(overInners, {
+            opacity: 1,
+            display: "block",
+            ease: "none",
+            duration: 0.6
+        }, 2)
+        timeline.call(()=>{
+            setShowTopBottom(true)
+        })
+        timeline.call(()=>{
+            setShowMiddle(true)
+        })
     }, [])
 
     return(
         <>
-            <div className={s.preloader__overlay}>
-                <div className={s.overlay__top} ref={topBlackRef}></div>
-                <div className={s.overlay__bottom} ref={bottomBlackRef}></div>
-            </div>
             <div className={s.preloader}>
-                <Marquee className={s.preloader__marquee} speed={90} direction={"right"} >
-                    <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
-                    <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
-                    <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
-                </Marquee>
-                <Marquee className={cn(s.preloader__marquee, s.white)} speed={90}>
-                    <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
-                    <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
-                    <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
-                </Marquee>
-                <Marquee className={s.preloader__marquee} speed={90} direction={"right"} ref={bottomBlackRef}>
-                    <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
-                    <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
-                    <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
-                </Marquee>
+                <div className={s.overlay__top} ref={topBlackRef}>
+                    <div className={s.overlay__top__inner} ref={addMarqueeRef}>
+                        {showTopBottom && (
+                            <Marquee className={s.preloader__marquee} speed={90} direction={"right"} >
+                                <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
+                                <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
+                                <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
+                            </Marquee>
+                        )}
+                    </div>
+                </div>
+                <div className={s.overlay__middle} ref={middleRef}>
+                    <div className={s.overlay__center__inner} ref={addMarqueeRef}>
+                        {showMiddle && (<Marquee className={cn(s.preloader__marquee, s.white)} speed={90} >
+                            <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
+                            <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
+                            <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
+                        </Marquee>)}
+                    </div>
+                </div>
+               
+                <div className={s.overlay__bottom} ref={bottomBlackRef}>
+                    <div className={s.overlay__bottom__inner} ref={addMarqueeRef} > 
+                        {showTopBottom && (
+                            <Marquee className={s.preloader__marquee} speed={90} direction={"right"} >
+                                <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
+                                <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
+                                <h1 className={s.preloader__heading} ref={addMultiRef}><span>Kove</span></h1>
+                            </Marquee>
+                            )}
+                    </div>
+                </div>
+            
             </div>
         </>
     )
